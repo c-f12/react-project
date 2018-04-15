085be6952333f3f9b2e0264772e172dfcf06bab2
Rafa Ramírez <raffe90@gmail.com>
2018-04-16 01:10:36 +0200

    Basic config & integration w react-router-redux@next

---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,9 @@
+import * as types from '../types/movies'
+import initialState from './initialState'
+
+export default function moviesReducer(state = initialState.movies, action){
+    switch(action.type){
+    default:
+      return state
+  }
+}
